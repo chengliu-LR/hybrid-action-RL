568246a8898ee1128ed2d82c1d755ab169da64ca
Cheng <c.liu-10@tudelft.nl>
2021-03-26 12:50:51 +0100

    final plots for pdqn and mpdqn

--- plots_goalEnv/tsplot_pdqn.py
+++ plots_goalEnv/tsplot_pdqn.py
@@ -9,13 +9,19 @@ data_mpdqn_3 = np.loadtxt("../logs/log_pdqn_GoalEnv_seed_28.txt", dtype=np.float
 data_mpdqn_4 = np.loadtxt("../logs/log_pdqn_GoalEnv_seed_37.txt", dtype=np.float32, delimiter=',')
 data_mpdqn_5 = np.loadtxt("../logs/log_pdqn_GoalEnv_seed_98.txt", dtype=np.float32, delimiter=',')
 
-data_pdqn_1 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_14.txt", dtype=np.float32, delimiter=',')
-data_pdqn_2 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_44.txt", dtype=np.float32, delimiter=',')
-data_pdqn_3 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_69.txt", dtype=np.float32, delimiter=',')
-data_pdqn_4 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_72.txt", dtype=np.float32, delimiter=',')
-data_pdqn_5 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_80.txt", dtype=np.float32, delimiter=',')
+# data_pdqn_1 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_14.txt", dtype=np.float32, delimiter=',')
+# data_pdqn_2 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_44.txt", dtype=np.float32, delimiter=',')
+# data_pdqn_3 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_69.txt", dtype=np.float32, delimiter=',')
+# data_pdqn_4 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_72.txt", dtype=np.float32, delimiter=',')
+# data_pdqn_5 = np.loadtxt("../logs/log_pdqn_single_GoalEnv_seed_80.txt", dtype=np.float32, delimiter=',')
 
-column = 4
+data_pdqn_1 = np.loadtxt("../logs/pdqn_single_GoalEnv_seed5.txt", dtype=np.float32, delimiter=',')
+data_pdqn_2 = np.loadtxt("../logs/pdqn_single_GoalEnv_seed29.txt", dtype=np.float32, delimiter=',')
+data_pdqn_3 = np.loadtxt("../logs/pdqn_single_GoalEnv_seed49.txt", dtype=np.float32, delimiter=',')
+data_pdqn_4 = np.loadtxt("../logs/pdqn_single_GoalEnv_seed70.txt", dtype=np.float32, delimiter=',')
+data_pdqn_5 = np.loadtxt("../logs/pdqn_single_GoalEnv_seed92.txt", dtype=np.float32, delimiter=',')
+
+column = 3
 
 #seaborn.tsplot(data_pdqn[:,2], color='orange', legend=True)
 training_data_mpdqn = np.array((data_mpdqn_1[:,column], data_mpdqn_2[:,column], data_mpdqn_3[:,column], data_mpdqn_4[:,column], data_mpdqn_5[:,column]))
@@ -26,8 +32,24 @@ plt.figure(dpi = 300)
 sb.tsplot(training_data_pdqn, color='coral')
 sb.tsplot(training_data_mpdqn, color='dodgerblue')
 plt.xlabel('Episodes (x10^2)')
-plt.ylabel('Probability of Success')
-plt.title('Success Probability on Training 20000 Episodes')
+plt.ylabel('Return over past 100 episodes')
+plt.title('Averaged return over past 100 episodes (training 50000 episodes)')
 plt.legend(labels=['p-dqn', 'mp-dqn'])
-plt.savefig("goal_env_success_rate_5_runs.png")
+plt.savefig("goal_env_averaged_return_5_runs.png")
+plt.close()
+
+column = 4
+
+training_data_mpdqn = np.array((data_mpdqn_1[:,column], data_mpdqn_2[:,column], data_mpdqn_3[:,column], data_mpdqn_4[:,column], data_mpdqn_5[:,column]))
+
+training_data_pdqn = np.array((data_pdqn_1[:,column], data_pdqn_2[:,column], data_pdqn_3[:,column], data_pdqn_4[:,column], data_pdqn_5[:,column]))
+
+plt.figure(dpi = 300)
+sb.tsplot(training_data_pdqn, color='coral')
+sb.tsplot(training_data_mpdqn, color='dodgerblue')
+plt.xlabel('Episodes (x10^2)')
+plt.ylabel('Success probability')
+plt.title('Success Probability (training 50000 episodes)')
+plt.legend(labels=['p-dqn', 'mp-dqn'])
+plt.savefig("goal_env_success_prob_5_runs.png")
 plt.close()
